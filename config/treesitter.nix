e2f4e6e2bf62fc490eba062dd1945dbb4bc2a1e9
Jamie Temple <jamie-temple@live.de>
2024-02-25 12:16:42 +0100

    chore(project): format files

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,10 +1,9 @@
-{pkgs, ...}: {
+{...}: {
   # https://nix-community.github.io/nixvim/plugins/treesitter/index.html
   plugins.treesitter = {
     enable = true;
     nixGrammars = true;
     indent = true;
-
   };
   plugins.treesitter-context = {
     enable = true;
